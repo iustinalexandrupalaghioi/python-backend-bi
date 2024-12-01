a6b8ab71076c309b2762ca1225b99775a137bb29
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-12-01 22:46:59 +0200

    modify script sql

--- create-tables-script.sql
+++ create-tables-script.sql
@@ -116,3 +116,26 @@ ALTER TABLE sales
 ADD COLUMN city_id INT REFERENCES cities(city_id) ON DELETE SET NULL;
 
 alter table clients add column age NUMERIC
+
+ALTER TABLE cities
+ADD COLUMN latitude NUMERIC(9, 6),
+ADD COLUMN longitude NUMERIC(9, 6);
+
+
+UPDATE cities SET latitude = 44.4268, longitude = 26.1025 WHERE city_name = 'Bucharest';
+UPDATE cities SET latitude = 46.7712, longitude = 23.6236 WHERE city_name = 'Cluj-Napoca';
+UPDATE cities SET latitude = 45.7489, longitude = 21.2087 WHERE city_name = 'Timișoara';
+UPDATE cities SET latitude = 47.1585, longitude = 27.6014 WHERE city_name = 'Iași';
+UPDATE cities SET latitude = 44.1598, longitude = 28.6348 WHERE city_name = 'Constanța';
+UPDATE cities SET latitude = 44.3302, longitude = 23.7949 WHERE city_name = 'Craiova';
+UPDATE cities SET latitude = 45.4353, longitude = 28.0074 WHERE city_name = 'Galați';
+UPDATE cities SET latitude = 44.9364, longitude = 26.0373 WHERE city_name = 'Ploiești';
+UPDATE cities SET latitude = 45.6438, longitude = 25.5887 WHERE city_name = 'Brașov';
+UPDATE cities SET latitude = 47.0722, longitude = 21.9214 WHERE city_name = 'Oradea';
+UPDATE cities SET latitude = 47.6342, longitude = 26.2592 WHERE city_name = 'Suceava';
+UPDATE cities SET latitude = 47.6573, longitude = 23.5681 WHERE city_name = 'Baia Mare';
+UPDATE cities SET latitude = 47.7921, longitude = 22.8857 WHERE city_name = 'Satu Mare';
+UPDATE cities SET latitude = 47.1416, longitude = 23.8759 WHERE city_name = 'Dej';
+UPDATE cities SET latitude = 47.3486, longitude = 25.3547 WHERE city_name = 'Vatra Dornei';
+UPDATE cities SET latitude = 47.5637, longitude = 25.8889 WHERE city_name = 'Gura Humorului';
+UPDATE cities SET latitude = 47.9531, longitude = 26.3973 WHERE city_name = 'Dorohoi';
